b5fd43098024eb1e0aacc462931a4ff74558232a
mikesae87 <mikesae87@gmail.com>
2025-09-20 12:12:33 -0400

    Use vite PWA config.
    Enables the application to be installed as a PWA.
    
    Configures Vite to use vite-plugin-pwa, generating a manifest and service worker.
    Moves icons and service worker script to the public directory.
    Removes the staticwebapp configuration file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/icon-192x192.png', 'icons/icon-512x512.png'], // Include icons
+      manifest: {
+        name: 'WineMapHead',
+        short_name: 'WineMap',
+        description: 'A Progressive Web App for WineMapHead',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
