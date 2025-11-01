52369373c4e55f7e9309ed1cd2a7eca8942643eb
mikesae87 <mikesae87@gmail.com>
2025-11-01 14:37:38 -0400

    Add legend; style with tailwind.

--- src/components/MapLegend.tsx
+++ src/components/MapLegend.tsx
@@ -3,33 +3,86 @@ import { vinsBlancs, vinsRouges } from "../types/legendColors"
 
 export const MapLegend: React.FC = () => {
     return (
-        <div className="map-legend">
-            <h4>Legend</h4>
-            <div>Vins Rouge</div>
-            <div>
-                Grand Cru
-                <svg className="legend-icon" width="20" height="20" focusable="false" xmlns="http://www.w3.org/2000/svg" fill={vinsRouges.GrandCru} viewBox="0 0 24 24" />
+        <div className="bg-white text-black absolute bottom-0 left-0 z-50 w-auto p-4 text-left border-gray-500 rounded-lg">
+            <div>Vin Rouge</div>
+            <div className="flex justify-between items-center">
+                <span className="ml-5 mr-10">Grand Cru</span>
+                <svg
+                    className="ml-auto"
+                    width="40"
+                    height="20"
+                    focusable="false"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <rect x="2" y="2" width="40" height="20" fill={vinsRouges.GrandCru} strokeWidth="3" stroke="#BBBBBB" />
+                </svg>
             </div>
-            <div>
-                Premier Cru
-                <svg className="legend-icon" width="20" height="20" focusable="false" xmlns="http://www.w3.org/2000/svg" fill={vinsRouges.PremierCru} viewBox="0 0 24 24" />
+            <div className="flex justify-between items-center">
+                <span className="ml-5 mr-10">Premier Cru</span>
+                <svg
+                    className="ml-auto"
+                    width="40"
+                    height="20"
+                    focusable="false"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <rect x="2" y="2" width="40" height="20" fill={vinsRouges.PremierCru} strokeWidth="3" stroke="#BBBBBB" />
+                </svg>
             </div>
-            <div>
-                Village
-                <svg className="legend-icon" width="20" height="20" focusable="false" xmlns="http://www.w3.org/2000/svg" fill={vinsRouges.Village} viewBox="0 0 24 24" />
+            <div className="flex justify-between items-center">
+                <span className="ml-5 mr-10">Village</span>
+                <svg
+                    className="ml-auto"
+                    width="40"
+                    height="20"
+                    focusable="false"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <rect x="2" y="2" width="40" height="20" fill={vinsRouges.Village} strokeWidth="3" stroke="#BBBBBB" />
+                </svg>
             </div>
             <div>Vin Blanc</div>
-            <div>
-                Grand Cru
-                <svg className="legend-icon" width="20" height="20" focusable="false" xmlns="http://www.w3.org/2000/svg" fill={vinsBlancs.GrandCru} viewBox="0 0 24 24" />
+            <div className="flex justify-between items-center">
+                <span className="ml-5 mr-10">Grand Cru</span>
+                <svg
+                    className="ml-auto"
+                    width="40"
+                    height="20"
+                    focusable="false"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <rect x="2" y="2" width="40" height="20" fill={vinsBlancs.GrandCru} strokeWidth="3" stroke="#BBBBBB" />
+                </svg>
             </div>
-            <div>
-                Premier Cru
-                <svg className="legend-icon" width="20" height="20" focusable="false" xmlns="http://www.w3.org/2000/svg" fill={vinsBlancs.PremierCru} viewBox="0 0 24 24" />
+            <div className="flex justify-between items-center">
+                <span className="ml-5 mr-10">Premier Cru</span>
+                <svg
+                    className="ml-auto"
+                    width="40"
+                    height="20"
+                    focusable="false"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <rect x="2" y="2" width="40" height="20" fill={vinsBlancs.PremierCru} strokeWidth="3" stroke="#BBBBBB" />
+                </svg>
             </div>
-            <div>
-                Village
-                <svg className="legend-icon" width="20" height="20" focusable="false" xmlns="http://www.w3.org/2000/svg" fill={vinsBlancs.Village} viewBox="0 0 24 24" />
+            <div className="flex justify-between items-center">
+                <span className="ml-5 mr-10">Village</span>
+                <svg
+                    className="ml-auto"
+                    width="40"
+                    height="20"
+                    focusable="false"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                >
+                    <rect x="2" y="2" width="40" height="20" fill={vinsBlancs.Village} strokeWidth="3" stroke="#BBBBBB" />
+                </svg>
             </div>
         </div>
     )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in the `src` directory
+    "./index.html",        // Include your HTML file(s) if applicable
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
