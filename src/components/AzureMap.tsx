071dfa5e2be3dac013b8028be1e2b35f6a43ec87
mikesae87 <mikesae87@gmail.com>
2025-11-11 21:13:47 -0500

    Add rudimentary infotool

--- src/components/AzureMap.tsx
+++ src/components/AzureMap.tsx
@@ -1,6 +1,6 @@
 import atlas from "azure-maps-control";
 import 'azure-maps-control/dist/atlas.min.css';
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { addDataSource, addFeatureSet, addFillTemplates, addPlacesLabelLayer, addSymbolLayer } from "../data-processing/add-functions";
 import type { AzureMapProps } from "../types/mapping";
 import events from "./events";
@@ -12,11 +12,15 @@ const AzureMap: React.FC<AzureMapProps> = ({ markers, regions, places }) => {
     useEffect(() => {
         if (!mapRef.current) return;
         const map = createMap(mapRef.current);
+        const popup = new atlas.Popup({
+            position: [0, 0],
+            pixelOffset: [0, -18]
+        });
+        map.popups.add(popup);
 
         map.events.add('mouseup', (e: atlas.MapMouseEvent) => {
             const pixel = e.pixel || [0, 0];
             const latLong = map.pixelsToPositions([pixel]);
-            console.log('Clicked at latitude/longitude:', latLong[0]);
 
             // lookup shapes at this position
             const shapes = map.layers.getRenderedShapes(latLong[0]);
@@ -25,6 +29,35 @@ const AzureMap: React.FC<AzureMapProps> = ({ markers, regions, places }) => {
                     console.log('Shape properties:', shape.getProperties());
                 }
             })
+
+            const topShape: any = shapes.length > 0 ? shapes[0] : null;
+
+            // check top shape for properties
+            if (typeof topShape.getProperties !== 'function') {
+                popup.close();
+                return;
+            }
+
+            const props = topShape.getProperties();
+            const appellation = props && props.hasOwnProperty('appellation') ? props['appellation'] : '';
+            const aocLevel = props && props.hasOwnProperty('aoc_level') ? props['aoc_level'] : '';
+            const climat = props && props.hasOwnProperty('climat') ? props['climat'] : '';
+
+            // if no relevant properties, close popup and exit
+            if (!appellation && !aocLevel && !climat) {
+                popup.close();
+                return;
+            }
+
+            popup.setOptions({
+                position: latLong[0],
+                content: `<div class="p-4 text-black flex flex-col items-start">
+                            <h3>${appellation}</h3>
+                            <h3>${climat}</h3>
+                            <h3>${aocLevel}</h3>
+                          </div>`
+            });
+            popup.open(map);
         });
 
         map.events.add("ready", async () => {
